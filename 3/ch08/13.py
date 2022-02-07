9443681781c1fc2b98f464997b5d44ba88128706
hermes7144 <kichancho@gmail.com>
2022-02-07 18:09:11 +0900

    선형리스트

---
+++ 3/ch08/13.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        q: List = []
+
+        if not head:
+            return True
+
+        node = head
+        # 리스트 변환
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+
+        # 팰린드롬 판별
+        while len(q) > 1:
+            if q.pop(0) != q.pop():
+                return False
+
+        return True
+s1 = Solution()
+prices = [7,1,5,3,6,4]
+print(s1.isPalindrome(prices))
+
+            
+
+
+        
