da42f09a871c7ea8193e8d92d9e4cc1655c707f3
kichan <kichancho@gmail.com>
2022-02-07 23:09:45 +0900

    2

--- 3/ch08/13.py
+++ 3/ch08/13.py
@@ -1,33 +1,48 @@
 from typing import List
+from typing import Optional
+import collections
 
-
+# Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+   def __init__(self, val=0, next=None):
+       self.val = val
+       self.next = next
+ 
 
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
-        q: List = []
-
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        vals = []
+        current_node = head
+        print(head)
+        while current_node is not None:
+            vals.append(current_node.val)
+            current_node = current_node.next
+        return vals == vals[::-1]
+    def isPalindrome2(self, head: Optional[ListNode]) -> bool:
+        # 데크자료형 선언
+        q: Deque = collections.deque()
+        
         if not head:
             return True
 
         node = head
-        # 리스트 변환
         while node is not None:
             q.append(node.val)
             node = node.next
-
-        # 팰린드롬 판별
         while len(q) > 1:
-            if q.pop(0) != q.pop():
+            if q.popleft() != q.pop():
                 return False
-
         return True
+            
 s1 = Solution()
-prices = [7,1,5,3,6,4]
-print(s1.isPalindrome(prices))
+
+list1 = ListNode(1);
+list1.next = ListNode(2);
+list1.next.next = ListNode(2);
+list1.next.next.next = ListNode(1);
+
+print(list1)
+print(s1.isPalindrome2(list1));
 
             
 
