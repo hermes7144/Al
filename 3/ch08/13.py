0e2f4f4b99c8b3090841ed45769fe8086935f1b8
hermes7144 <kichancho@gmail.com>
2022-02-08 21:06:32 +0900

    연결리스트 병합

--- 3/ch08/13.py
+++ 3/ch08/13.py
@@ -33,7 +33,20 @@ class Solution:
             if q.popleft() != q.pop():
                 return False
         return True
-            
+    class Solution:
+        def isPalindrome3(self, head: Optional[ListNode]) -> bool:
+            rev =None;
+            slow = fast = head
+            while fast and fast.next:
+                fast = fast.next.next;
+                rev, rev.next, slow = slow, rev, slow.next
+            if fast:
+                slow = slow.next
+            #
+            while rev and rev.val == slow.val:
+                slow, rev = slow.next, rev.next
+            return not rev
+
 s1 = Solution()
 
 list1 = ListNode(1);
